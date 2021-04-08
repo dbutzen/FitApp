a8c3c7e30da3a3d0ca8825806a51f223fc86c730
Daniel Butzen <300057079@fvtc.edu>
2021-04-08 19:22:31 +0000

    Updated tblUser.sql

--- TCT.FitApp/TCT.FitApp.DB/Tables/tblUser.sql
+++ TCT.FitApp/TCT.FitApp.DB/Tables/tblUser.sql
@@ -1,4 +1,12 @@
 ﻿CREATE TABLE [dbo].[tblUser]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Username] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [CalorieGoal] INT NOT NULL, 
+    [ProteinGoal] INT NOT NULL, 
+    [DaysInARowSucceeded] INT NOT NULL, 
+    [HeightInches] INT NOT NULL, 
+    [WeightPounds] INT NOT NULL
 )
