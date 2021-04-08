d2a7444776ef6e2633876b32ddfc23832d52c640
Daniel Butzen <300057079@fvtc.edu>
2021-04-08 19:21:55 +0000

    Updated tblDay.sql

--- TCT.FitApp/TCT.FitApp.DB/Tables/tblDay.sql
+++ TCT.FitApp/TCT.FitApp.DB/Tables/tblDay.sql
@@ -1,4 +1,10 @@
 ﻿CREATE TABLE [dbo].[tblDay]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [UserId] UNIQUEIDENTIFIER NOT NULL, 
+    [Date] DATE NOT NULL, 
+    [CaloriesEaten] INT NOT NULL, 
+    [Protein] INT NOT NULL, 
+    [Succeeded] BIT NOT NULL, 
+    [CaloriesBurned] INT NOT NULL
 )
