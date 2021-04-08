50237b19255f481f6426ccb19dfb5a827e60db80
Daniel Butzen <300057079@fvtc.edu>
2021-04-08 19:22:10 +0000

    Updated tblItem.sql

--- TCT.FitApp/TCT.FitApp.DB/Tables/tblItem.sql
+++ TCT.FitApp/TCT.FitApp.DB/Tables/tblItem.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[tblItem]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Type] VARCHAR(50) NOT NULL, 
+    [Calories] INT NOT NULL, 
+    [Protein] INT NOT NULL
 )
