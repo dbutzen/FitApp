2c27ca53760f5d213bc37c8c852449b345ede54d
Daniel Butzen <300057079@fvtc.edu>
2021-04-20 19:51:58 -0400

    DayActivity and DayItem managers

--- TCT.FitApp/TCT.FitApp.BL/DayActivityManager.cs
+++ TCT.FitApp/TCT.FitApp.BL/DayActivityManager.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TCT.FitApp.BL.Models;
+using TCT.FitApp.PL;
 
 namespace TCT.FitApp.BL
 {
-    class DayActivityManager
+    public static class DayActivityManager
     {
+        public static async Task<int> Insert(Guid dayId, Guid activityId, int duration, int difficultyLevel, bool rollback = false)
+        {
+            try
+            {
+                IDbContextTransaction transaction = null;
+                using (FitAppEntities dc = new FitAppEntities())
+                {
+                    if (rollback) transaction = dc.Database.BeginTransaction();
+                    TblDayActivity row = new TblDayActivity();
+                    row.Id = Guid.NewGuid();
+                    row.ActivityId = activityId;
+                    row.DayId = dayId;
+                    row.Duration = duration;
+                    row.DifficultyLevel = difficultyLevel;
+                    dc.TblDayActivities.Add(row);
+                    int results = dc.SaveChanges();
+                    if (rollback) transaction.Rollback();
+                    return results;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static async Task<int> Delete(Guid id, bool rollback = false)
+        {
+            IDbContextTransaction transaction = null;
+            using (FitAppEntities dc = new FitAppEntities())
+            {
+                if (rollback == true) transaction = dc.Database.BeginTransaction();
+                TblDayActivity row = dc.TblDayActivities.FirstOrDefault(qa => qa.Id == id);
+                int results = 0;
+                if (row != null)
+                {
+                    dc.TblDayActivities.Remove(row);
+                    results = dc.SaveChanges();
+
+                    if (rollback) transaction.Rollback();
+                    return results;
+                }
+                else
+                {
+                    throw new Exception("Row was not found.");
+                }
+            }
+        }
+        
     }
 }
--- TCT.FitApp/TCT.FitApp.BL/DayItemManager.cs
+++ TCT.FitApp/TCT.FitApp.BL/DayItemManager.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TCT.FitApp.BL.Models;
+using TCT.FitApp.PL;
 
 namespace TCT.FitApp.BL
 {
-    class DayItemManager
+    public static class DayItemManager
     {
+        public static async Task<int> Insert(Guid dayId, Guid itemId, int servings, bool rollback = false)
+        {
+            try
+            {
+                IDbContextTransaction transaction = null;
+                using (FitAppEntities dc = new FitAppEntities())
+                {
+                    if (rollback) transaction = dc.Database.BeginTransaction();
+                    TblDayItem row = new TblDayItem();
+                    row.Id = Guid.NewGuid();
+                    row.ItemId = itemId;
+                    row.DayId = dayId;
+                    row.Servings = servings;
+                    dc.TblDayItems.Add(row);
+                    int results = dc.SaveChanges();
+                    if (rollback) transaction.Rollback();
+                    return results;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static async Task<int> Delete(Guid id, bool rollback = false)
+        {
+            IDbContextTransaction transaction = null;
+            using (FitAppEntities dc = new FitAppEntities())
+            {
+                if (rollback == true) transaction = dc.Database.BeginTransaction();
+                TblDayItem row = dc.TblDayItems.FirstOrDefault(qa => qa.Id == id);
+                int results = 0;
+                if (row != null)
+                {
+                    dc.TblDayItems.Remove(row);
+                    results = dc.SaveChanges();
+
+                    if (rollback) transaction.Rollback();
+                    return results;
+                }
+                else
+                {
+                    throw new Exception("Row was not found.");
+                }
+            }
+        }
+
     }
 }
