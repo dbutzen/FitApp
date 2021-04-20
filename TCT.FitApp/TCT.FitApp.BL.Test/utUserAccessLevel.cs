e70195ed7d61d37ad2e006d0dcc6f7dfa51a7a7a
Chris Van Hefty <300058841@fvtc.edu>
2021-04-19 22:32:50 -0500

    Added UserAccessLevelManager and BL Tests.

---
+++ TCT.FitApp/TCT.FitApp.BL.Test/utUserAccessLevel.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using TCT.FitApp.BL;
+using TCT.FitApp.BL.Models;
+
+namespace TCT.FitApp.BL.Test
+{
+    [TestClass]
+    public class utUserAccessLevel
+    {
+        [TestMethod]
+        public void LoadTest()
+        {
+            var task = UserAccessLevelManager.Load();
+            task.Wait();
+            var results = task.Result;
+            Assert.AreEqual(3, results.Count);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            var task = UserAccessLevelManager.Insert(new UserAccessLevel { Name = "Compliance", Description = "Read/Write/Modify" }, true);
+            task.Wait();
+            Assert.IsTrue(task.Result > 0);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var task = UserAccessLevelManager.Load();
+            task.Wait();
+            var userAccessLevels = task.Result;
+            UserAccessLevel userAccessLevel = userAccessLevels.FirstOrDefault(ual => ual.Name == "Super User");
+            userAccessLevel.Name = "Superhuman User";
+            var results = UserAccessLevelManager.Update(userAccessLevel, true);
+            results.Wait();
+            Assert.IsTrue(results.Result > 0);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            var task = UserAccessLevelManager.Load();
+            task.Wait();
+            var userAccessLevels = task.Result;
+            UserAccessLevel userAccessLevel = userAccessLevels.FirstOrDefault(ual => ual.Name == "Super User");
+            var results = UserAccessLevelManager.Delete(userAccessLevel.Id, true);
+            results.Wait();
+            Assert.IsTrue(results.Result > 0);
+        }
+
+        [TestMethod]
+        public void LoadByNameTest()
+        {
+            var task = UserAccessLevelManager.LoadByName("Super User");
+            task.Wait();
+            var results = task.Result;
+            Assert.AreEqual("Modify", results.Description);
+        }
+
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+            var task = UserAccessLevelManager.LoadByName("Super User");
+            task.Wait();
+            var id = task.Result.Id;
+
+            task = UserAccessLevelManager.LoadById(id);
+            task.Wait();
+            var results = task.Result;
+            Assert.AreEqual(id, results.Id);
+        }
+    }
+}
--- TCT.FitApp/TCT.FitApp.BL/UserAccessLevelManager.cs
+++ TCT.FitApp/TCT.FitApp.BL/UserAccessLevelManager.cs
@@ -1,12 +1,206 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using TCT.FitApp.BL.Models;
+using TCT.FitApp.PL;
 
 namespace TCT.FitApp.BL
 {
-    class UserAccessLevelManager
+    public class UserAccessLevelManager
     {
+        public async static Task<int> Insert(UserAccessLevel userAccessLevel, bool rollback = false)
+        {
+            try
+            {
+                var results = 0;
+                await Task.Run(() =>
+                {
+                    IDbContextTransaction transaction = null;
+
+                    using (var dc = new FitAppEntities())
+                    {
+                        if (rollback) transaction = dc.Database.BeginTransaction();
+
+                        var row = new TblUserAccessLevel();
+
+                        row.Id = Guid.NewGuid();
+                        row.Name = userAccessLevel.Name;
+                        row.Description = userAccessLevel.Description;
+
+                        userAccessLevel.Id = row.Id;
+
+                        dc.TblUserAccessLevels.Add(row);
+
+                        results = dc.SaveChanges();
+
+                        if (rollback) transaction.Rollback();
+                    }
+                });
+
+                return results;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public async static Task<int> Update(UserAccessLevel userAccessLevel, bool rollback = false)
+        {
+            try
+            {
+                var results = 0;
+                await Task.Run(() =>
+                {
+                    IDbContextTransaction transaction = null;
+
+                    using (var dc = new FitAppEntities())
+                    {
+                        if (rollback) transaction = dc.Database.BeginTransaction();
+
+                        var row = dc.TblUserAccessLevels.FirstOrDefault(ual => ual.Id == userAccessLevel.Id);
+
+                        row.Name = userAccessLevel.Name;
+                        row.Description = userAccessLevel.Description;
+
+                        results = dc.SaveChanges();
+
+                        if (rollback) transaction.Rollback();
+                    }
+                });
+
+                return results;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async static Task<int> Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                var results = 0;
+                await Task.Run(() =>
+                {
+                    IDbContextTransaction transaction = null;
+
+                    using (var dc = new FitAppEntities())
+                    {
+                        if (rollback) transaction = dc.Database.BeginTransaction();
+
+                        var row = dc.TblUserAccessLevels.FirstOrDefault(ual => ual.Id == id);
+                        dc.TblUserAccessLevels.Remove(row);
+                        results = dc.SaveChanges();
+
+                        if (rollback) transaction.Rollback();
+                    }
+                });
+
+                return results;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async static Task<List<UserAccessLevel>> Load()
+        {
+            try
+            {
+                List<UserAccessLevel> userAccessLevels = new List<UserAccessLevel>();
+
+                await Task.Run(() =>
+                {
+                    using (var dc = new FitAppEntities())
+                    {
+                        dc.TblUserAccessLevels
+                            .ToList()
+                            .ForEach(ual => userAccessLevels.Add(new UserAccessLevel
+                            {
+                                Id = ual.Id,
+                                Name = ual.Name,
+                                Description = ual.Description,
+                            }));
+                    }
+                });
+
+                return userAccessLevels;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async static Task<UserAccessLevel> LoadByName(String name)
+        {
+            try
+            {
+                UserAccessLevel userAccessLevel = new UserAccessLevel();
+                await Task.Run(() =>
+                {
+                    using (FitAppEntities dc = new FitAppEntities())
+                    {
+                        TblUserAccessLevel tblUserAccessLevel = dc.TblUserAccessLevels.FirstOrDefault(ual => ual.Name == name);
+
+                        if (tblUserAccessLevel != null)
+                        {
+                            userAccessLevel.Id = tblUserAccessLevel.Id;
+                            userAccessLevel.Name = tblUserAccessLevel.Name;
+                            userAccessLevel.Description = tblUserAccessLevel.Description;
+                        }
+                        else
+                        {
+                            throw new Exception("Could not find the row.");
+                        }
+                    }
+                });
+                return userAccessLevel;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async static Task<UserAccessLevel> LoadById(Guid id)
+        {
+            try
+            {
+                UserAccessLevel userAccessLevel = new UserAccessLevel();
+                await Task.Run(() =>
+                {
+                    using (FitAppEntities dc = new FitAppEntities())
+                    {
+                        TblUserAccessLevel tblUserAccessLevel = dc.TblUserAccessLevels.FirstOrDefault(ual => ual.Id == id);
+
+                        if (tblUserAccessLevel != null)
+                        {
+                            userAccessLevel.Id = tblUserAccessLevel.Id;
+                            userAccessLevel.Name = tblUserAccessLevel.Name;
+                            userAccessLevel.Description = tblUserAccessLevel.Description;
+                        }
+                        else
+                        {
+                            throw new Exception("Could not find the row.");
+                        }
+                    }
+                });
+                return userAccessLevel;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
