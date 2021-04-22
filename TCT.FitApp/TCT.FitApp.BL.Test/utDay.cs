39c9d8d449adacbe457c612c265c13c5ca7c3953
Chris Van Hefty <300058841@fvtc.edu>
2021-04-21 23:03:21 -0500

    Added utDay and added line to DayManager LoadById to add day to day list

--- TCT.FitApp/TCT.FitApp.BL.Test/utDay.cs
+++ TCT.FitApp/TCT.FitApp.BL.Test/utDay.cs
@@ -21,5 +21,74 @@ namespace TCT.FitApp.BL.Test
             Assert.AreEqual(2, task.Result.Count);
         }
 
+        [TestMethod]
+        public void LoadTest()
+        {
+            var task = DayManager.Load();
+            task.Wait();
+            var results = task.Result;
+            Assert.AreEqual(4, results.Count);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var taskd = DayManager.Load();
+            taskd.Wait();
+            var days = taskd.Result;
+
+            Day day = days.FirstOrDefault(d => d.Date == DateTime.Parse("03-12-21"));
+            day.Succeeded = true;
+
+            var task = DayManager.Update(day, true);
+            task.Wait();
+            Assert.IsTrue(task.Result > 0);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            var taskd = DayManager.Load();
+            taskd.Wait();
+            var days = taskd.Result;
+
+            Day day = days.FirstOrDefault(d => d.Date == DateTime.Parse("03-12-21"));
+
+            var task = DayManager.Delete(day.Id, true);
+            task.Wait();
+            Assert.IsTrue(task.Result > 0);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            var utask = UserManager.LoadByUsername("cvanhefty");
+            utask.Wait();
+            var userId = utask.Result.Id;
+
+            Day day = new Day();
+            day.Id = Guid.NewGuid();
+            day.UserId = userId;
+            day.Succeeded = false;
+            day.Date = DateTime.Parse("04-21-21");
+
+            var task = DayManager.Insert(day, true);
+            task.Wait();
+            Assert.AreEqual(task.Result, true);
+        }
+
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+            var dtask = DayManager.Load();
+            dtask.Wait();
+            var days = dtask.Result;
+
+            var day = days.FirstOrDefault(d => d.Date == DateTime.Parse("03-12-21"));
+
+            var results = DayManager.LoadById(day.Id);
+            results.Wait();
+            Assert.AreEqual(1, results.Result.Count);
+        }
     }
 }
