a9c2a86a750f3fd7e8b1f4b3bda633207b69a521
Jason Ryan <300057524@fvtc.edu>
2021-04-26 17:53:17 -0500

    Added ItemController and utItem tests.

---
+++ TCT.FitApp/TCT.FitApp.API.Test/utItem.cs
@@ -0,0 +1,157 @@
+﻿using TCT.FitApp.BL.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System;
+
+namespace TCT.FitApp.API.Test
+{
+    [TestClass]
+    public class utItem
+    {
+        public HttpClient client { get; }
+
+        public utItem()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<TCT.FitApp.API.Startup>();
+
+            var testServer = new TestServer(webHostBuilder);
+
+            client = testServer.CreateClient();
+        }
+
+
+        private List<Item> GetItems()
+        {
+            HttpResponseMessage response;
+            string result;
+            dynamic items;
+
+            response = client.GetAsync("Item").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            items = (JArray)JsonConvert.DeserializeObject(result);
+            List<Item> itemList = items.ToObject<List<Item>>();
+
+            return itemList;
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<Item> items = GetItems();
+
+            Assert.AreEqual(4, items.Count);
+
+        }
+
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+            HttpResponseMessage response;
+            string result;
+
+            var id = GetItems().FirstOrDefault(i => i.Name == "Cauliflower").Id;
+
+            response = client.GetAsync($"Item/{id}").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            var item = JsonConvert.DeserializeObject<Item>(result);
+
+
+            Assert.AreEqual("Cauliflower", item.Name);
+
+        }
+
+        [TestMethod]
+        public void LoadByNameTest()
+        {
+            HttpResponseMessage response;
+            string result;
+
+            var name = "Cauliflower";
+            response = client.GetAsync($"Item/{name}").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            var item = JsonConvert.DeserializeObject<Item>(result);
+
+
+            Assert.AreEqual(name, item.Name);
+
+        }
+
+
+        // ItemTypeController is required
+        //[TestMethod]
+        public void LoadByTypeId()
+        {
+            HttpResponseMessage response;
+            string result;
+
+            var typeId = Guid.Empty;  // ItemTypeController is required
+            response = client.GetAsync($"Item/Type/{typeId}").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            var item = JsonConvert.DeserializeObject<Item>(result);
+
+
+            Assert.AreEqual(typeId, item.Id);
+
+        }
+
+        // ItemTypeController is required
+        //[TestMethod]
+        public void InsertTest()
+        {
+            var item = new Item();
+            item.Name = "New Item";
+            //item.TypeId = 
+            item.Calories = 10;
+            item.Protein = 7;
+
+            var serializedObject = JsonConvert.SerializeObject(item);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var response = client.PostAsync("Item?rollback=true", content).Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var item = GetItems().FirstOrDefault(i => i.Name == "Cauliflower");
+            item.Name = "Updated Name";
+
+            var serializedObject = JsonConvert.SerializeObject(item);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var response = client.PutAsync($"Item/{item.Id}?rollback=true", content).Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            var id = GetItems().FirstOrDefault(i => i.Name == "Cauliflower").Id;
+
+            var response = client.DeleteAsync($"Item/{id}?rollback=true").Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+
+    }
+}
---
+++ TCT.FitApp/TCT.FitApp.API/Controllers/ItemController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TCT.FitApp.BL;
+using TCT.FitApp.BL.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TCT.FitApp.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        /// <summary>
+        /// Return a list of items
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> Get()
+        {
+            try
+            {
+                return Ok(await ItemManager.Load());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get a item by itemname
+        /// </summary>
+        /// <param name="itemname"></param>
+        /// <returns></returns>
+        [HttpGet("{itemName}")]
+        public async Task<ActionResult<Item>> Get(string itemName)
+        {
+            try
+            {
+                return Ok(await ItemManager.LoadByName(itemName));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get an item by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult<Item>> Get(Guid id)
+        {
+            try
+            {
+                return Ok(await ItemManager.LoadById(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get a list of items by type id
+        /// </summary>
+        /// <param name="typeId"></param>
+        /// <returns></returns>
+        [HttpGet("Type/{typeId:Guid}")]
+        public async Task<ActionResult<IEnumerable<Item>>> GetByTypeId(Guid typeId)
+        {
+            try
+            {
+                return Ok(await ItemManager.LoadByTypeId(typeId));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Insert a new item
+        /// </summary>
+        /// <param name="Item"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Item Item, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await ItemManager.Insert(Item, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Update an item
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="Item"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] Item Item, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await ItemManager.Update(Item, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Delete an item
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await ItemManager.Delete(id, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
