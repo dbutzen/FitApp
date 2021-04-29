07d8ce6defc4302983c4af730f2980de2255b46c
Daniel Butzen <300057079@fvtc.edu>
2021-04-29 17:07:46 -0400

    ActivityController, utActivity for API

---
+++ TCT.FitApp/TCT.FitApp.API.Test/utActivity.cs
@@ -0,0 +1,122 @@
+﻿using TCT.FitApp.BL.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System;
+
+namespace TCT.FitApp.API.Test
+{
+    [TestClass]
+    public class utActivity
+    {
+        public HttpClient client { get; }
+
+        public utActivity()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<TCT.FitApp.API.Startup>();
+
+            var testServer = new TestServer(webHostBuilder);
+
+            client = testServer.CreateClient();
+        }
+
+
+        private List<Activity> GetActivities()
+        {
+            HttpResponseMessage response;
+            string result;
+            dynamic activities;
+
+            response = client.GetAsync("Activity").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            activities = (JArray)JsonConvert.DeserializeObject(result);
+            List<Activity> activityList = activities.ToObject<List<Activity>>();
+
+            return activityList;
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<Activity> activities = GetActivities();
+
+            Assert.AreEqual(3, activities.Count);
+
+        }
+
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+            HttpResponseMessage response;
+            string result;
+
+            var id = GetActivities().FirstOrDefault(i => i.Name == "Running").Id;
+
+            response = client.GetAsync($"Activity/{id}").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            var activity = JsonConvert.DeserializeObject<Activity>(result);
+
+
+            Assert.AreEqual("Running", activity.Name);
+
+        }
+
+        
+        public void InsertTest()
+        {
+            var activity = new Activity();
+            activity.Name = "Eating";
+            activity.EasyCaloriesPerHour = 10;
+            activity.MediumCaloriesPerHour = 15;
+            activity.HardCaloriesPerHour = 20;
+
+            var serializedObject = JsonConvert.SerializeObject(activity);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var response = client.PostAsync("Activity?rollback=true", content).Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var activity = GetActivities().FirstOrDefault(i => i.Name == "Running");
+            activity.Name = "Running Faster";
+
+            var serializedObject = JsonConvert.SerializeObject(activity);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var response = client.PutAsync($"Activity/{activity.Id}?rollback=true", content).Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            var id = GetActivities().FirstOrDefault(i => i.Name == "Running").Id;
+
+            var response = client.DeleteAsync($"Activity/{id}?rollback=true").Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+
+    }
+}
---
+++ TCT.FitApp/TCT.FitApp.API/Controllers/ActivityController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TCT.FitApp.BL;
+using TCT.FitApp.BL.Models;
+
+namespace TCT.FitApp.API.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        /// <summary>
+        /// Return a list of items
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BL.Models.Activity>>> Get()
+        {
+            try
+            {
+                return Ok(await ActivityManager.Load());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Get an item by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult<BL.Models.Activity>> Get(Guid id)
+        {
+            try
+            {
+                return Ok(await ActivityManager.LoadById(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        
+        /// <summary>
+        /// Insert a new item
+        /// </summary>
+        /// <param name="Activity"></param>
+        /// <param name="rollback"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] BL.Models.Activity Activity, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await ActivityManager.Insert(Activity, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Update an item
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="Activity"></param>
+        /// <param name="rollback"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] BL.Models.Activity Activity, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await ActivityManager.Update(Activity, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Delete an item
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="rollback"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await ActivityManager.Delete(id, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
+
+
