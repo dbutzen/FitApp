1f9ae6c30a7be0750f1428df40f918f9532f2ce2
Daniel Butzen <300057079@fvtc.edu>
2021-04-29 18:11:06 -0400

    UserAccessLevel controller and unit test for API

---
+++ TCT.FitApp/TCT.FitApp.API.Test/utUserAccessLevel.cs
@@ -0,0 +1,135 @@
+﻿using TCT.FitApp.BL.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System;
+
+namespace TCT.FitApp.API.Test
+{
+    [TestClass]
+    public class utUserAccessLevel
+    {
+        public HttpClient client { get; }
+
+        public utUserAccessLevel()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<TCT.FitApp.API.Startup>();
+
+            var testServer = new TestServer(webHostBuilder);
+
+            client = testServer.CreateClient();
+        }
+
+
+        private List<UserAccessLevel> GetUserAccessLevels()
+        {
+            HttpResponseMessage response;
+            string result;
+            dynamic userAccessLevels;
+
+            response = client.GetAsync("UserAccessLevel").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            userAccessLevels = (JArray)JsonConvert.DeserializeObject(result);
+            List<UserAccessLevel> userAccessLevelList = userAccessLevels.ToObject<List<UserAccessLevel>>();
+
+            return userAccessLevelList;
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<UserAccessLevel> userAccessLevels = GetUserAccessLevels();
+
+            Assert.AreEqual(3, userAccessLevels.Count);
+
+        }
+
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+            HttpResponseMessage response;
+            string result;
+
+            var id = GetUserAccessLevels().FirstOrDefault(i => i.Name == "Admin").Id;
+
+            response = client.GetAsync($"UserAccessLevel/{id}").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            var userAccessLevel = JsonConvert.DeserializeObject<UserAccessLevel>(result);
+
+
+            Assert.AreEqual("Admin", userAccessLevel.Name);
+
+        }
+
+        [TestMethod]
+        public void LoadByNameTest()
+        {
+            HttpResponseMessage response;
+            string result;
+
+            var name = "Admin";
+            response = client.GetAsync($"UserAccessLevel/{name}").Result;
+            result = response.Content.ReadAsStringAsync().Result;
+            var item = JsonConvert.DeserializeObject<Item>(result);
+
+
+            Assert.AreEqual(name, item.Name);
+
+        }
+
+        public void InsertTest()
+        {
+            var userAccessLevel = new UserAccessLevel();
+            userAccessLevel.Name = "Slug";
+            userAccessLevel.Description = "Also Slug";
+
+            var serializedObject = JsonConvert.SerializeObject(userAccessLevel);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var response = client.PostAsync("UserAccessLevel?rollback=true", content).Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var userAccessLevel = GetUserAccessLevels().FirstOrDefault(i => i.Name == "Admin");
+            userAccessLevel.Name = "Overlord";
+
+            var serializedObject = JsonConvert.SerializeObject(userAccessLevel);
+            var content = new StringContent(serializedObject);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var response = client.PutAsync($"UserAccessLevel/{userAccessLevel.Id}?rollback=true", content).Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            var id = GetUserAccessLevels().FirstOrDefault(i => i.Name == "Admin").Id;
+
+            var response = client.DeleteAsync($"UserAccessLevel/{id}?rollback=true").Result;
+            var result = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+            Assert.IsTrue(result > 0);
+
+        }
+
+
+    }
+}
---
+++ TCT.FitApp/TCT.FitApp.API/Controllers/UserAccessLevelController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TCT.FitApp.BL;
+using TCT.FitApp.BL.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TCT.FitApp.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserAccessLevelController : ControllerBase
+    {
+        /// <summary>
+        /// Return a list of userAccessLevels
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserAccessLevel>>> Get()
+        {
+            try
+            {
+                return Ok(await UserAccessLevelManager.Load());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get an userAccessLevel by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("{name}")]
+        public async Task<ActionResult<UserAccessLevel>> Get(string name)
+        {
+            try
+            {
+                return Ok(await UserAccessLevelManager.LoadByName(name));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get an userAccessLevel by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult<UserAccessLevel>> Get(Guid id)
+        {
+            try
+            {
+                return Ok(await UserAccessLevelManager.LoadById(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Insert a new userAccessLevel
+        /// </summary>
+        /// <param name="UserAccessLevel"></param>
+        /// <param name="rollback"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserAccessLevel UserAccessLevel, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await UserAccessLevelManager.Insert(UserAccessLevel, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Update an userAccessLevel
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="UserAccessLevel"></param>
+        /// <param name="rollback"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] UserAccessLevel UserAccessLevel, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await UserAccessLevelManager.Update(UserAccessLevel, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Delete an userAccessLevel
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="rollback"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                return Ok(await UserAccessLevelManager.Delete(id, rollback));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
