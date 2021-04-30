103df4af01f7772f368040f53a42266986564f43
Jason Ryan <300057524@fvtc.edu>
2021-04-29 21:36:54 -0500

    Added Nutritionix test UI

---
+++ TCT.FitApp/Nutritionix/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Nutritionix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nutritionix
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void btnSubmitUPC_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var item = await Manager.GetByUPC(txtUPC.Text);
+                txtItemName.Text = item.item_name;
+                txtItemProtein.Text = item.nf_protein.ToString();
+                txtItemCalories.Text = item.nf_calories.ToString();
+            }
+            catch { }
+
+        }
+
+        private async void btnSubmitName_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var items = await Manager.GetByName(txtKeyword.Text);
+                dgItems.ItemsSource = null;
+                dgItems.ItemsSource = items;
+                //txtItemName.Text = item.item_name;
+                //txtItemProtein.Text = item.nf_protein.ToString();
+                //txtItemCalories.Text = item.nf_calories.ToString();
+            }
+            catch(Exception) { throw; }
+        }
+    }
+}
---
+++ TCT.FitApp/Nutritionix/Manager.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Nutritionix.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Nutritionix
+{
+    public static class Manager
+    {
+        private static string GetBaseUri()
+        {
+            return "https://nutritionix-api.p.rapidapi.com/v1_1/";
+        }
+        public static async Task<Item> GetByUPC(string upc)
+        {
+            try
+            {
+                string result;
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"{GetBaseUri()}item?upc={upc}"),
+                    Headers =
+                            {
+                                { "x-rapidapi-key", "35be3e7930msh6d601bfd8008df4p136ce8jsna6728d5864d6" },
+                                { "x-rapidapi-host", "nutritionix-api.p.rapidapi.com" },
+                            }
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    result = await response.Content.ReadAsStringAsync();
+                }
+                var item = JsonConvert.DeserializeObject<Item>(result);
+                return item;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static async Task<List<Item>> GetByName(string name)
+        {
+            try
+            {
+                var items = new List<Item>();
+                string result;
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"{GetBaseUri()}search/{name.Replace(" ","%20").Replace(",", "%20")}?fields=item_name%2Citem_description%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_protein"),
+                    Headers =
+                            {
+                                { "x-rapidapi-key", "35be3e7930msh6d601bfd8008df4p136ce8jsna6728d5864d6" },
+                                { "x-rapidapi-host", "nutritionix-api.p.rapidapi.com" },
+                            }
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    result = await response.Content.ReadAsStringAsync();
+                }
+                var reponse = JsonConvert.DeserializeObject<Response>(result);
+
+                foreach (var h in reponse.hits)
+                {
+                    items.Add(h.fields);
+                }
+                return items;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
